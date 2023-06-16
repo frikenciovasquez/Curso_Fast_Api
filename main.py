1242959ae22c5c5c51fb2d400375ff8b86da1ac6
Daniel Vasquez <sanda9397@gmail.com>
2023-06-16 18:25:15 -0500

    metodos post,put y delete, explicacion de parametros de ruta y parametros query

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+
 from fastapi.responses import HTMLResponse
 
 
@@ -20,7 +21,7 @@ movies = [
         'overview': "Ahora los pitufos estan mas enojados",
         'year': '2023',
         'rating': 6.5,
-        'category': 'Acción'    
+        'category': 'fantasia'    
     } 
 ]
 @app.get('/',tags=['home'])
@@ -42,8 +43,44 @@ def get_movie(id: int):
 
 @app.get('/movies/', tags=['movies'])
 def get_movies_by_category(category : str):
-        for item in movies:
+        
+        return [item for item in movies if item['category'] ==category]
+        '''  for item in movies:
             if item['category'] == category:
-                return item
+                return item'''
+          
+# metodo post:
 
+@app.post('/movies',tags=['movies'])
+def create_movies(id: int = Body(),title:str=Body(),overwiew:str=Body(),year:int=Body(),rating:float=Body(),category:str=Body()):
+     movies.append({
+          "id":id,
+          "title":title,
+          "overwiew":overwiew,
+          "year":year,
+          "rating":rating,
+          "category":category
+          })
+     return movies
+     
+     
+@app.put('/movies/{id}',tags=['movies'])
+def update_movie(id: int,title:str=Body(),overwiew:str=Body(),year:int=Body(),rating:float=Body(),category:str=Body()):
+     for item in movies:
+          if item['id']==id:
+            item['title']=title,
+            item['overwiew']=overwiew,
+            item['year']=year,
+            item['rating']=rating,
+            item['category']=category
+            return movies
+
+@app.delete('/movies/{id}',tags=['movies'])
+def delete_movie(id:int):
+        for item in movies:
+          if item['id']==id:
+               movies.remove(item)
+               return movies
+                 
+        
     
